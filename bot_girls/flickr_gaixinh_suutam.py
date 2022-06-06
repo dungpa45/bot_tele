a327bc2c3a644774ae5dea839113e83ffe1a77e9
dungpa45 <dphamanh45@gmail.com>
2022-06-06 23:02:38 +0700

    restructure

--- bot_girls/flickr_gaixinh_suutam.py
+++ bot_girls/flickr_gaixinh_suutam.py
@@ -1,22 +1,54 @@
-from telegram.ext import Updater, CommandHandler, InlineQueryHandler, MessageHandler, Filters
-from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent, ReplyKeyboardMarkup
 import glob
-import random
-import os
-from flickrapi import FlickrAPI
-import json
+import os, json, random
+import pickle
 import requests
-import random
+import redis
 from yaml import Loader
 from yaml import load
+from flickrapi import FlickrAPI
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler, MessageHandler, Filters
+from telegram import ParseMode,  ReplyKeyboardMarkup
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from google.auth.transport.requests import Request
+
 
 with open("./secret.yaml","r") as yml_file:
     data = load(yml_file, Loader=Loader)
 
+# connect redis
+HOST = data["redis"]["host"]
+PASSWORD = data["redis"]["pass"]
+redis_server = redis.Redis(host=HOST, port=6379, db=1, password=PASSWORD)
+all_keys = redis_server.keys()
+
+def get_all_key_redis():
+    all_keys = redis_server.keys()
+    all_keys = [key.decode("utf-8") for key in all_keys]
+    print(all_keys)
+    return all_keys
+
+def save_in_redis(d_value):
+    print(type(d_value))
+    try:
+        with redis_server.pipeline() as pipe:
+            # save key in redis
+            pipe.set("girl",d_value)
+            pipe.execute()
+        redis_server.bgsave()
+    except Exception as e:
+        print("loi ko luu dc",e)
+    print("save done")
+
+def get_data_redis(str_key):
+    # d_b_value = redis_server.hgetall(str_key)
+    s_b_value = redis_server.get(str_key)
+    s_value = s_b_value.decode("utf-8")
+    l_value = json.loads(s_value)
+    return l_value
+
 reply_keboard = [['/girl', '/woman'],['/vsbg', '/sexygirl'], ['/gaingon', '/xinh']]
 markup = ReplyKeyboardMarkup(reply_keboard, one_time_keyboard=True)
-
-CHOOSE = 1
 
 def get_requestURL(user_id, endpoint="getList"):
     user_id = user_id.replace("@", "%40")
@@ -69,6 +101,75 @@ for photoset_id, title in zip(id_album, titles_album):  # for each album
 
     d_album_img[photoset_id] = urls
 
+def get_google_album():
+    print("get image from gg")
+    scopes = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+
+    creds = None
+    CREDENTIALS_FILE = 'credentials.pickle'
+
+    if os.path.exists(CREDENTIALS_FILE):
+        with open(CREDENTIALS_FILE, 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json',
+                scopes)
+            creds = flow.run_local_server(port=8090)
+            with open(CREDENTIALS_FILE, 'wb') as token:
+                pickle.dump(creds, token)
+
+    service = build('photoslibrary', 'v1', credentials=creds,static_discovery=False)
+
+    albums_shared = service.sharedAlbums().list(
+        pageSize=10).execute()
+
+    list_album = albums_shared.get('sharedAlbums', [])
+    # print(list_album)
+    for album in list_album:
+        if album["title"] == "gái xinh":
+            album_id = album["id"]
+
+    nextpagetoken = 'Dummy'
+    c=0
+    list_item=[]
+    while nextpagetoken != '':
+        print("wait.....")
+        nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
+        results = service.mediaItems().search(
+                body={"albumId": album_id,
+                    "pageSize": 100, "pageToken": nextpagetoken}).execute()
+        # The default number of media items to return at a time is 25. The maximum pageSize is 100.
+        items = results.get('mediaItems', [])
+        nextpagetoken = results.get('nextPageToken', '')
+        for item in items:
+            c+=1
+            # print(f"{c}\nURL: {item['productUrl']}")
+            list_item.append(item['baseUrl'])
+            # l.append(item['productUrl'])
+    return list_item
+
+def get_girl_img():
+    print("check redis first")
+    list_keys = get_all_key_redis()
+    if list_keys == []:
+        print("Chua co trong redis")
+        l_img = get_google_album()
+        img = random.choice(l_img)
+        print("gg",img)
+        s_l_img = json.dumps(l_img)
+        save_in_redis(s_l_img)
+        return img
+    else:
+        print("Da co trong redis")
+        l_img = get_data_redis("girl")
+        img = random.choice(l_img)
+        print("redis",img)
+        return img
 
 def get_image_local(img_dir):
     data_path = os.path.join(img_dir, '*.jpg')
@@ -77,67 +178,46 @@ def get_image_local(img_dir):
     img = img_.replace('\\', '/')
     return img
 
-
 def get_id_album(id_album):
     l_img = d_album_img[id_album]
     return l_img
-
 
 def get_vsbg_img():
     l_img = get_id_album(d_title_id_album[titles_album[1]])
     img = random.choice(l_img)
     return img
 
-
-def get_girl_img():
-    l_img = get_id_album(d_title_id_album[titles_album[2]])
-    img = random.choice(l_img)
-    return img
-
+# def get_girl_img():
+#     l_img = get_id_album(d_title_id_album[titles_album[2]])
+#     img = random.choice(l_img)
+#     return img
 
 def get_korea_img():
     l_img = get_id_album(d_title_id_album[titles_album[0]])
     img = random.choice(l_img)
     return img
 
-
 def get_gaitay_img():
     l_img = get_id_album(d_title_id_album[titles_album[3]])
     img = random.choice(l_img)
     return img
 
-
-def get_multi_vsbg_img():
-    l_img = get_id_album(d_title_id_album["vsbg 8-1-2020"])
-    img = random.choice(l_img)
-    return img
-
-
-def get_multi_girl_img():
-    l_img = get_id_album(d_title_id_album["GXCL 8-1-2020"])
-    # li_img = get_id_album(d_title_id_album["MNTH 8-1-2020"])
-    img = random.choice(l_img)
-    return img
-
-
 def start(bot, update):
     user = update.message.from_user
     update.message.reply_text(
-        "Hello master {} \nType /gái /xinh /gaixinh /gaingon /sexygirl /girl /lady /vsbg /woman  to see random picture\nHave fun :)".format(
+        "Hello {} \nType /gái /xinh /gaixinh /gaingon /sexygirl /girl /lady /vsbg /woman  to see random picture\nHave fun :)".format(
             user.full_name),
         reply_markup=markup)
-
 
 def help(bot, update):
     update.message.reply_text('''Note:
     /gái /girl /woman: Gái xinh chọn lọc
     /gaidep /lady /gaixinh : Gái rất xinh 
     /sexygirl /vsbg /sexylady : Gái xinh quyến rũ
-    /gaingon, /girlxinh , /xinh : Gái ngon hơn
-    /korean /korea /gáihàn: Gái Hàn Xẻng
+    /gaingon /girlxinh /xinh : Gái ngon hơn
+    /korean /korea /gáihàn /gaihan: Gái Hàn Xẻng
     /gaitay /gáitây: Gái Tây
     Have fun :)''')
-
 
 def girl(bot, update):
     girl = get_girl_img()
@@ -145,13 +225,11 @@ def girl(bot, update):
     mess_id = update.message.message_id
     bot.send_photo(chat_id=chat_id, reply_to_message_id=mess_id, photo=girl)
 
-
 def vsbg(bot, update):
     vsbg = get_vsbg_img()
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
     bot.send_photo(chat_id=chat_id, reply_to_message_id=mess_id, photo=vsbg)
-
 
 def korea(bot, update):
     korea = get_korea_img()
@@ -160,7 +238,6 @@ def korea(bot, update):
     bot.send_photo(chat_id=chat_id, reply_to_message_id=mess_id,
                    photo=open(korea, "rb"))
 
-
 def gaitay(bot, update):
     gaitay = get_gaitay_img()
     chat_id = update.message.chat_id
@@ -168,14 +245,12 @@ def gaitay(bot, update):
     bot.send_photo(chat_id=chat_id, reply_to_message_id=mess_id,
                    photo=open(gaitay, "rb"))
 
-
 def multi_girl(bot, update):
     l_girl = get_multi_girl_img()
     print(l_girl, type(l_girl))
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
     bot.send_photo(chat_id=chat_id, reply_to_message_id=mess_id, photo=l_girl)
-
 
 def multi_vsbg(bot, update):
     l_vsbg = get_multi_vsbg_img()
@@ -194,16 +269,14 @@ def time(bot, update, job_queue):
     job_queue.run_repeating()
 
 def main():
-    TOKEN = data["telegram"]["token_girl"]
+    TOKEN = data["telegram"]["token_quote"]
     updater = Updater(TOKEN)
     dp = updater.dispatcher
     start_handler = CommandHandler('start', start)
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler(["girl", "gái", "women", "woman"], girl))
-    # dp.add_handler(CommandHandler(["gaidep", "lady", "gaixinh"], multi_girl))
-    dp.add_handler(CommandHandler(['vsbg', 'sexygirl', 'sexylady'], vsbg))
-    # dp.add_handler(CommandHandler(['xinh', 'girlxinh', 'gaingon'], multi_vsbg))
-    dp.add_handler(CommandHandler(['korea', 'korean', 'gaihan'], korea))
+    dp.add_handler(CommandHandler(["girl", "gái","gaidep","girlxinh","lady","women", "woman","gaixinh","xinh"], girl))
+    dp.add_handler(CommandHandler(['vsbg', 'sexygirl', 'sexylady','gaingon'], vsbg))
+    dp.add_handler(CommandHandler(['korea', 'korean', 'gaihan',"gáihàn"], korea))
     dp.add_handler(CommandHandler(['gáitây', 'gaitay'], gaitay))
     dp.add_handler(MessageHandler(Filters.text, time, pass_job_queue=True))
 
@@ -212,7 +285,6 @@ def main():
     updater.start_polling()
     # Run the bot until press ctrl +C
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
