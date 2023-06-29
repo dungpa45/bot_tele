1e50d0fcd39a0288fce596764744ad684e5b3250
dphamanh45 <dphamanh45@gmail.com>
2023-06-29 10:32:20 +0700

    add cocktail...

--- linhtinh_aws_lambda.py
+++ linhtinh_aws_lambda.py
@@ -2,6 +2,7 @@ import json, traceback, os, io
 import requests, re, random
 import ipaddress
 from googletrans import Translator
+from requests.exceptions import HTTPError
 
 TOKEN = os.environ["TOKEN"]
 API_NINJA = os.environ["API_NINJA"]
@@ -25,8 +26,14 @@ def send_photo(image_url,chat_id,cap=None):
     if cap is None:
         tele_url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_id}"
     else:
-        tele_url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_id}&caption={cap}&parse_mode=Markdown"
-    requests.post(tele_url,files=files)
+        tele_url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_id}&caption={cap}"
+    try:
+        r=requests.post(tele_url,files=files)
+        r.raise_for_status()
+    except HTTPError as e:
+        url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}'
+        payload = {'text': "Có lỗi rồi:\n" + e.response.text}
+        requests.post(url,json=payload)
 
 def get_quote():
     res = requests.get("https://favqs.com/api/qotd")
@@ -75,11 +82,13 @@ def get_n_send_useless_fact(chat_id):
 def send_text(text_input,chat_id):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}'
     if "help" in text_input or "/help" in text_input or "/start" in text_input:
-        payload = {'text': '''Gõ /quote để xem một câu quote
-        \nGõ /fact để xem fact /uselessfact
-        \nGõ /meal để xem một món ăn ngẫu nhiên
-        \nGõ /trua_nay_an_gi để coi ăn cái chi
-        \nNhập đường link bất kỳ sẽ cho ra một link rút gọn'''}
+        payload = {'text': 
+'''Gõ /quote để xem một câu quote
+Gõ /fact để xem fact /uselessfact xem face vô tri
+Gõ /meal để xem một món ăn ngẫu nhiên
+Gõ /cocktail để xem một cốc têu ngẫu nhiên
+Gõ /an_trua /antrua để coi ăn cái chi
+Nhập đường link bất kỳ sẽ cho ra một link rút gọn'''}
     else:
         payload = {'text': "hổng hiểu gì hết trơn :)))\nGõ /help hoặc /start nha"}
     requests.post(url,json=payload)
@@ -169,11 +178,13 @@ def info_meals(dictionary):
         text = f'''Tên: {dictionary["strMeal"]}
 Loại: {dictionary["strCategory"]} 
 Khu vực: {dictionary["strArea"]}
-*Nguyên liệu:* 
-{nguyen_lieu}
+\nNguyên liệu:
+{translate_vn(nguyen_lieu)}
 Link Youtube: {dictionary["strYoutube"]}'''
         img_link = dictionary["strMealThumb"]
-        guide = f'''*Hướng dẫn:* {dictionary["strInstructions"]}
+        eng_guide = dictionary["strInstructions"]
+        vi_guide = translate_vn(eng_guide)
+        guide = f'''*Hướng dẫn:* {vi_guide}
 \nNguồn: {dictionary['strSource']}'''
     except Exception:
         traceback.print_exc()
@@ -193,13 +204,61 @@ def send_meals(chat_id):
         url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}'
         payload = {'parse_mode':'Markdown','text': l_info_meal[2]}
         requests.post(url,json=payload)
+    else:
+        error = "StatusCode: " + res.status_code +" "+ res.text
+        return error
 
+def info_drinks(dictionary):
+    # text = ''
+    m={}
+    d={}
+    for key, value in dictionary.items():   
+        if value in ['',' ',None]:
+            continue
+        else:   
+            if 'strIngredient' in key:
+                m.update({key:value})
+            elif 'strMeasure' in key:
+                new_key = key.replace('strMeasure','strIngredient')
+                d.update({new_key:value})
+    new_d = dict((m.get(k, k), v) for (k, v) in d.items())
+    nguyen_lieu = dict_to_text(new_d)
+    try:
+        text = f'''Tên: {dictionary["strDrink"]}
+Loại: {dictionary["strCategory"]} 
+Alcoholic: {dictionary["strAlcoholic"]}
+Cốc: {dictionary["strGlass"]}
+\nNguyên liệu:
+{translate_vn(nguyen_lieu)}'''
+        img_link = dictionary["strDrinkThumb"]
+        eng_guide = dictionary["strInstructions"]
+        vi_guide = translate_vn(eng_guide)
+        guide = f'''*Hướng dẫn:* {vi_guide}'''
+    except Exception:
+        traceback.print_exc()
+    return [text,img_link,guide]
+
+def send_cocktail(chat_id):
+    res = requests.get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    if res.status_code == requests.codes.ok:
+        l_res = res.json()
+        data_drink = l_res["drinks"][0]
+        l_info_drink = info_drinks(data_drink)
+        print(l_info_drink[0])
+        # url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}'
+        # payload = {'parse_mode':'Markdown','text': text_drink}
+        # requests.post(url,json=payload)
+        send_photo(l_info_drink[1],chat_id,l_info_drink[0])
+        url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}'
+        payload = {'parse_mode':'Markdown','text': l_info_drink[2]}
+        requests.post(url,json=payload)
     else:
         error = "StatusCode: " + res.status_code +" "+ res.text
         return error
 
 def an_trua(chat_id):
-    l_antrua = ['cơm rang', 'bún vịt', 'phở vịt quay', 'bún đậu', 'xôi', 'bánh mỳ', 'bún pò', 'cơm thố', 'phở bò']
+    l_antrua = ['cơm rang', 'bún vịt', 'phở vịt quay', 'bún đậu', 'xôi', 'bún chả'
+                 'bánh mỳ', 'bún pò', 'cơm thố', 'phở pò','cơm trộn','nem nướng']
     mon = random.choice(l_antrua)
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}'
     payload = {'parse_mode':'Markdown','text':mon}
@@ -239,7 +298,10 @@ def lambda_handler(event, context):
         elif "/meal" in user_text:
             send_meals(chat_id)
             return {"statusCode": 200}
-        elif "/trua_nay_an_gi" in user_text:
+        elif "/cocktail" in user_text:
+            send_cocktail(chat_id)
+            return {"statusCode": 200}
+        elif user_text in ["/trua_nay_an_gi","/an_trua","/antrua"]:
             an_trua(chat_id)
             return {"statusCode": 200}
         else:
